6a4301f68b2852db9b8ee65b24e2b6a88f0df24f
D3snik <ryandesnik45@gmail.com>
2024-07-08 10:14:58 -0300

    User insertion with POST

--- src/main/java/com/ryanlinhares/workshopmongo/services/UserService.java
+++ src/main/java/com/ryanlinhares/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ryanlinhares.workshopmongo.domain.User;
+import com.ryanlinhares.workshopmongo.dto.UserDTO;
 import com.ryanlinhares.workshopmongo.repository.UserRepository;
 import com.ryanlinhares.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -16,12 +17,22 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
 	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
